edee82f0d9efb9245117a2642d9c01d38ab5493f
rauanium <a.spark69@gmail.com>
2019-10-27 17:24:27 +0600

    commit from mac sdcl

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Contacs from './views/Contacs.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/contacs',
+      name: 'contacs',
+      component: Contacs
     },
     {
       path: '/about',
---
+++ src/views/Contacs.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="contacs">
+    <h1>This is an contacs page</h1>
+  </div>
+</template>
